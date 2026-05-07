283422087e0b0c0dbc349d69b8c562e16905d64b
IsaiahPressman <isaiahpressman16@gmail.com>
2026-05-07 13:27:23 -0400

    add agent utils

--- python/owl/agent/__init__.py
+++ python/owl/agent/__init__.py
@@ -1,8 +1,10 @@
 from .agent import Agent, AgentConfig
 from .kaggle_observation import KaggleObservation
+from .utils import find_checkpoint_path
 
 __all__ = [
     "Agent",
     "AgentConfig",
     "KaggleObservation",
+    "find_checkpoint_path",
 ]
